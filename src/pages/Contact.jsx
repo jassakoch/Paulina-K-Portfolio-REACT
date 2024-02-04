d533c8b54d22c97942620e8e23d68b18f021e5f9
paulina koch <pjassakoch@gmail.com>
2024-02-03 21:34:22 -0500

    Added utils helpers.js file for password validation, as well working on Contact page component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+
+
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from '../../utils/helpers';
+
+function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+//     // Based on the input type, we set the state of either name, email, message
+//     if (inputType === 'name') {
+//       setName(inputValue);
+//     } else if (inputType === 'userName') {
+//       setUserName(inputValue);
+//     } else {
+//       setPassword(inputValue);
+//     }
+//   };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the Name is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !Name) {
+      setErrorMessage('Email is invalid');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+ 
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div className="container text-center">
+      <h1>Contact Me</h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="Message"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+}
+
+export default Contact;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -21,28 +21,28 @@ export default function Portfolio() {
             title: "SVG Logo Maker",
             link: "https://drive.google.com/file/d/16mudgMov98j07hntR-rkLfIBdm5tsdA9/view",
             github: "https://github.com/jassakoch/SVG--Logo-Maker?tab=readme-ov-file",
-            // image: //path to public folder
+            image: "./SVG-Logo.png"
     },
 
         {
             title: "Text Editor - PWA",
             link: "https://jate-text-editor-z9be.onrender.com/",
             github: "https://github.com/jassakoch/Text-Editor?tab=readme-ov-file",
-            // image: //path to public folder
+            image: "./JATE-text-editor.png"
     },
 
         {
             title: "Social Network",
             link: "https://app.screencastify.com/v3/watch/9klagsUYKJXQWHrUZ8Uc",
             github: "https://github.com/jassakoch/Social-Network?tab=readme-ov-file",
-            // image: //path to public folder
+            image:"./Social_Network.png"
     },
 
         {
             title: "Note Taker",
             link: "https://note-taker-jassa-d1c823307af1.herokuapp.com/notes",
             github: "https://github.com/jassakoch/Note-Taker?tab=readme-ov-file",
-            // image: //path to public folder
+            image:"./Note-Taker.png"
     }
 
 
