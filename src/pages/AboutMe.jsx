d5e0690fd36312885c05c47986466e1f52704374
paulina koch <pjassakoch@gmail.com>
2024-02-04 20:10:53 -0500

    Added to AboutMe component

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,10 @@
 export default function AboutMe () {
 return (
-    <h1>I am Paulina i love kotlet</h1>
+    <div>
+    <h1>Paulina Koch</h1>
+    <h3>I transform your <span> ideas</span> into digital <span>reality</span></h3>
+    <p> As an elementary school teacher and a devoted mom, I embarked on a transformative journey into the world of technology. With unwavering determination and nights spent mastering coding languages, I emerged as a proficient full stack developer. Now, I seamlessly blend my educational insights with technical expertise, creating innovative beatiful applications.</p>
+    <img src="./Paulina_profile.JPG" alt="Paulina's profile"></img>
+    </div>
 )
 };
